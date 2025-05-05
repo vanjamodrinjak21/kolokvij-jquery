1f2e72177d7f7b2a52ee38cccd87a47d646e4b35
vanjamodrinjak21 <adynamics21@gmail.com>
2025-05-05 19:59:29 +0200

    Dodani jQueryUI tabovi i funkcija prosjek

--- skripte.js
+++ skripte.js
@@ -1,1 +1,10 @@
-// JavaScript code for the project 
+// JavaScript code for the project
+
+$( function() {
+    $( "#tabs" ).tabs();
+} );
+
+function prosjek(br1, br2, br3, br4) {
+    const suma = br1 + br2 + br3 + br4;
+    return suma / 4;
+} 
